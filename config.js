0e7fb873353c24986d256a7f4f2b76b0dca0deae
Mika Raento <mikie@iki.fi>
2014-05-18 16:24:17 +0300

    protractor browser reuse example

---
+++ config.js
@@ -0,0 +1,11 @@
+requirejs.config({
+  shim: {
+    angular: {
+      exports: "angular"
+    },
+  },
+  paths: {
+    requirejs: "bower_components/requirejs/require",
+    angular: "bower_components/angular/angular"
+  }
+});
---
+++ index.js
@@ -0,0 +1,18 @@
+define(['angular'], function(angular, $, bootstrap_slider) {
+
+var app = angular.module('bower-helloworld', []);
+
+if (location.href.indexOf("protractor-test") < 0) {
+    angular.bootstrap(document.getElementById("app"), ['bower-helloworld']);
+} else {
+    angular.element(document).ready(function() {
+        var elem = document.getElementById("app");
+        window.name = 'NG_DEFER_BOOTSTRAP!' + window.name;
+        angular.bootstrap(elem, ['bower-helloworld']);
+        elem.setAttribute('ng-app', '');
+        window.angular = angular;
+    });
+}
+
+});
+
